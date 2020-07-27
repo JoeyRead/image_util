009d9e01fa2aa2e74f97fd8772cfa6d70e93110a
AbrarNitk <abrar.nitk@gmail.com>
2020-07-27 08:46:26 +0530

    image resize post and get api

--- app.js
+++ app.js
@@ -8,46 +8,7 @@ const image_resize = require('./routes/resize');
 const port = 3000;
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
-const success = function (res, data) {
-    return res.status(200).json({success: true, data: data});
-};
-
-const error_response = function (res, error, status = 500) {
-    return res.status(status).json({success: false, error: error});
-};
-
-//Upload route
-app.post('/upload', utils.upload.single('image'), (req, res, next) => {
-    try {
-        const width = parseInt(req.query.width);
-        const height = parseInt(req.query.height);
-        if (isNaN(req.query.width) || isNaN(width) || width == null) {
-            return error_response(res, "width should not be null", 400);
-        }
-
-        if (isNaN(req.query.height) || isNaN(height) || height == null) {
-            return error_response(res, "height should not be null", 400);
-        }
-
-        sharp(req.file.path).resize({height: height, width: width})
-            .toFile('uploads/' + 'thumbnails-' + req.file.originalname, (err, resizeImage) => {
-                if (err) {
-                    console.log(err);
-                    return error_response(res, err);
-                } else {
-                    console.log(resizeImage);
-                }
-            });
-        return res.status(201).json({
-            message: 'File uploded successfully'
-        });
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-app.use('/image', image_resize);
+app.use('/api/image', image_resize);
 app.get('/', (req, res) => {
     res.status(200).send("Hello World");
 });
--- routes/resize.js
+++ routes/resize.js
@@ -1,11 +1,80 @@
 const express = require('express');
 const router = express.Router();
 const image_service = require('../service/image_resize');
+const sharp = require('sharp');
+const utils = require('../util/utils');
+
+
+const success = function (res, data) {
+    return res.status(200).json({success: true, data: data});
+};
+
+const error_response = function (res, error, status = 500) {
+    return res.status(status).json({success: false, error: error});
+};
+
+router.post('/resize', utils.upload.single('image'), function (req, res) {
+
+    const width = parseInt(req.query.width);
+    const height = parseInt(req.query.height);
+
+    if (isNaN(req.query.width) || isNaN(width) || width == null) {
+        return error_response(res, "width should not be null", 400);
+    }
+
+    if (isNaN(req.query.height) || isNaN(height) || height == null) {
+        return error_response(res, "height should not be null", 400);
+    }
+    try {
+        sharp(req.file.path).resize({height: height, width: width})
+            .toFile('uploads/' + 'thumbnails-' + req.file.originalname, (err, resizeImage) => {
+                if (err) {
+                    console.log(err);
+                    return error_response(res, err);
+                } else {
+                    console.log(resizeImage);
+                }
+            });
+        return success(res, "file path");
+    } catch (error) {
+        console.error(error);
+        return error_response(res, error, 500);
+    }
+});
 
 router.get('/resize', function (req, res) {
-    res.json({
-        'hello': 'world'
-    })
+    const width = parseInt(req.query.width);
+    const height = parseInt(req.query.height);
+    const file_path = req.query.path;
+
+    if (isNaN(req.query.width) || isNaN(width) || width == null) {
+        return error_response(res, "width should not be null", 400);
+    }
+
+    if (isNaN(req.query.height) || isNaN(height) || height == null) {
+        return error_response(res, "height should not be null", 400);
+    }
+
+    if(file_path == null) {
+        return error_response(res, "file path is mandatory", 400);
+    }
+
+    try {
+        sharp(file_path).resize({height: height, width: width})
+            .toFile('uploads/' + 'thumbnails-' + '1.jpg', (err, resizeImage) => {
+                if (err) {
+                    console.log(err);
+                    return error_response(res, err);
+                } else {
+                    console.log(resizeImage);
+                }
+            });
+        return success(res, "file path");
+    } catch (error) {
+        console.error(error);
+        return error_response(res, error, 500);
+    }
+
 });
 
 module.exports = router;
